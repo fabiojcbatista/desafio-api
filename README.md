6312f318e12471ee0943c5e86df1ddf99b9f0df3
fabiojcbatista <fabiojcbweb@gmail.com>
2022-05-20 19:07:35 -0300

    Finalização testes de API

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# desafio-api
-Desafio de teste de API
+# Desafio Api
+
+![GitHub repo size](https://img.shields.io/github/repo-size/fabiojcbatista/desafio-api?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/fabiojcbatista/desafio-api?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/fabiojcbatista/desafio-api?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/fabiojcbatista/desafio-api?style=for-the-badge)
+![Bitbucket open pull requests](https://img.shields.io/bitbucket/pr-raw/fabiojcbatista/desafio-api?style=for-the-badge)
+
+Você deverá utilizar a seguinte API para realizer o desafio :
+
+> https://apidetarefas.docs.apiary.io/#reference/0/contatos
+
+Passo-a-passo:
+
+> 1. Você deverá Listar, Criar , Editar e Deletar um contato.
+> 2. Todo funcionamento da API esta na sua documentação.
+> 3. Use o ambiente “Production” da API
+
+
+### Ajustes e melhorias
+
+O projeto ainda está em desenvolvimento e as próximas atualizações serão voltadas nas seguintes tarefas:
+
+- [x] Criar os casos de testes no google planilhas
+- [x] Criar a Base de Testes
+- [x] Criar os testes
+
+## 💻 Pré-requisitos
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- Uma máquina `<Windows>`. compatível na versão 10.
+- Java compatível com a versão 1.8.
+- IDE de desenvolvimento `<VS Code/Eclipse/Intellij Idea>`
+
+## 🚀 Instalando
+
+Para instalar as dependências, siga estas etapas:
+
+
+Java 8
+
+> [Clique aqui para fazer o download do Java 8](https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246471_2dee051a5d0647d5be72a7c0abff270e)
+
+Eclipse
+
+> [Clique aqui para fazer o download do Eclipse](https://www.eclipse.org/downloads/)
+
+VS Code
+
+> [Clique aqui para fazer o download do VS Code](https://code.visualstudio.com/download)
+
+Intellij Idea
+
+> [Clique aqui para fazer o download do Intellij](https://www.jetbrains.com/pt-br/idea/download/)
+
+## ☕ Usando desafio-api
+
+Para usar desafio-api, siga estas etapas:
+
+Clone ou baixe o projeto para uma pasta local
+
+> Clone:
+
+```
+git clone https://github.com/fabiojcbatista/desafio-api.git
+```
+
+> Depois abra na sua IDE preferida: Vs Code/Eclipse/Intellij Idea
+> Para testar use o comando:
+
+```
+mvn test
+```
+
+## 📫 Contribuindo para desafio-api
+
+Para contribuir com desafio-api, siga estas etapas:
+
+1. Bifurque este repositório.
+2. Crie um branch: `git checkout -b <nome_branch>`.
+3. Faça suas alterações e confirme-as: `git commit -m '<mensagem_commit>'`
+4. Envie para o branch original: `git push origin <nome_do_projeto> / <local>`
+5. Crie a solicitação de pull.
+
+Como alternativa, consulte a documentação do GitHub em [como criar uma solicitação pull](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr>
+    <td align="center">Fabio Junior C. Batista</td>
+
+  </tr>
+</table>
+
+## 😄 Seja um dos contribuidores<br>
--- src/test/java/br/com/desafio/test/ContatoTest.java
+++ src/test/java/br/com/desafio/test/ContatoTest.java
@@ -1,28 +1,237 @@
 package br.com.desafio.test;
 
+import static io.restassured.RestAssured.given;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 
 @RunWith(DataProviderRunner.class)
 public class ContatoTest {
 
- @DataProvider
- public static Object[][] contatos() {
-     return new Object[][] {
-         { "", "Aparecido", "j.aparecido@gmail.com","16","51994533283","Rua Floriano Peixoto, 100", "RS", "Viamão" },
-         { "us", "12345", "Schenectady" },
-         { "ca", "B2R", "Waverley"},
-         {"nl", "1001", "Amsterdam"}
-     };
- }
+    private String dados = "{ \"name\": \"\",\"last_name\": \"Santos\",\"email\": \"andersen30@gmail.com\",\"age\": \"75\",\"phone\": \"51994533283\",\"address\": \"Rua Flores\",\"state\": \"RS\",\"city\": \"Canoas\"}";
 
+    @DataProvider
+    public static Object[][] contatos() {
+        return new Object[][] {
+                { "Luiz", "Aparecido", "j1.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "", "Aparecido", "j2.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "9999", "Aparecido", "j3.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "#@$%", "Aparecido", "j4.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { " ", "Aparecido", "j5.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "", "j6.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "9999", "j7.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "&%$#", "j8.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", " ", "j9.aparecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.a1parecidogmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "Viamão" },
+                { "Anderson", "Aparecido", "j.a2parecido@gmail", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "", "16", "51994533283", "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.a3parecido@gmail.com", "@#", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "Viamão" },
+                { "Anderson", "Aparecido", "j.a4parecido@gmail.com", " ", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "Viamão" },
+                { "Anderson", "Aparecido", "j.a5parecido@gmail.com", "idade", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "Viamão" },
+                { "Anderson", "Aparecido", "j.a6parecido@gmail.com", "16", "@#$",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.a7parecido@gmail.com", "16", " ",
+                        "Rua Floriano Peixoto, 100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.a8parecido@gmail.com", "16", "51994533283", "100", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.a9parecido@gmail.com", "16", "51994533283", "@#%$", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.ap1arecido@gmail.com", "16", "51994533283", " ", "RS",
+                        "Viamão" },
+                { "Anderson", "Aparecido", "j.ap2arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "51", "Viamão" },
+                { "Anderson", "Aparecido", "j.ap3arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "#@$", "Viamão" },
+                { "Anderson", "Aparecido", "j.ap4arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        " ", "Viamão" },
+                { "Anderson", "Aparecido", "j.ap5arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "9999" },
+                { "Anderson", "Aparecido", "j.ap6arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", "#2%@" },
+                { "Anderson", "Aparecido", "j.ap7arecido@gmail.com", "16", "51994533283",
+                        "Rua Floriano Peixoto, 100",
+                        "RS", " " }
 
- @Test
- @UseDataProvider("contatos")
- public void ct001_ct020_naoDeveCadastrarCasosDeTesteComDadosInvalidos(String countryCode, String zipCode, String expectedPlaceName) 
- {
+        };
+    }
 
-}
+    @DataProvider
+    public static Object[][] listaExclusao() {
+        return new Object[][] {
+                { "1736" },
+                { "1737" },
+                { "1738" },
+                { "1739" },
+                { "1740" },
+                { "1741" },
+                { "1742" },
+                { "1743" },
+                { "1734" },
+                { "1735" },
+                { "1736" },
+                { "1747" },
+                { "1748" },
+                { "1749" },
+                { "1750" },
+                { "1755" },
+                { "1756" },
+                { "1757" },
+                { "1758" },
+                { "1759" },
+                { "1760" },
+                { "1761" },
+                { "1762" },
+                { "1763" }
+
+        };
+    }
+
+    @Test
+    // @UseDataProvider("contatos")
+    public void ct001_deveListarTodosOsContatos() {
+        given()
+                .log()
+                .all()
+                .when()
+                .get("https://api-de-tarefas.herokuapp.com/contacts")
+                .then()
+                .log()
+                .body()
+                .statusCode(200);
+    }
+
+    @Test
+    @UseDataProvider("contatos")
+    public void ct003_ct027_naoDeveCadastrarUmNovoContato(String name, String last_name, String email, String age,
+            String phone, String address, String state, String city) {
+        given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .body("{ \"name\": \"" + name + "\",\"last_name\": \"" + last_name + "\",\"email\": \"" + email
+                        + "\",\"age\": \"" + age + "\",\"phone\":\"" + phone + "\",\"address\": \"" + address
+                        + "\",\"state\": \"" + state + "\",\"city\":\" " + city + "\"}")
+                .when()
+                .post("https://api-de-tarefas.herokuapp.com/contacts")
+                .then()
+                .log()
+                .body()
+                .statusCode(422);
+    }
+
+    @Test
+    public void ct002_deveCadastrarUmNovoContato() {
+        given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .body(dados)
+                .when()
+                .post("https://api-de-tarefas.herokuapp.com/contacts")
+                .then()
+                .log()
+                .body()
+                .statusCode(201);
+    }
+
+    @Test
+    public void ct068_deveAlterarUmNovoContato() {
+        given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .body(dados)
+                .when()
+                .put("https://api-de-tarefas.herokuapp.com/contacts/441")
+                .then().statusCode(200);
+
+    }
+
+    @Test
+    @UseDataProvider("contatos")
+    public void ct069_ct093_naoDeveAlterarUmNovoContato(String name, String last_name, String email, String age,
+            String phone, String address, String state, String city) {
+        given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .body("{ \"name\": \"" + name + "\",\"last_name\": \"" + last_name + "\",\"email\": \"" + email
+                        + "\",\"age\": \"" + age + "\",\"phone\":\"" + phone + "\",\"address\": \"" + address
+                        + "\",\"state\": \"" + state + "\",\"city\":\" " + city + "\"}")
+                .when()
+                .put("https://api-de-tarefas.herokuapp.com/contacts/441")
+                .then()
+                .log()
+                .body()
+                .statusCode(422);
+    }
+
+    @Test
+    @UseDataProvider("listaExclusao")
+    public void ct135_deveExcluirContatoExistente(String id) {
+        given()
+                .log()
+                .all()
+                .pathParam("id", id)
+                .when()
+                .delete("https://api-de-tarefas.herokuapp.com/contacts/{id}")
+                .then()
+                .log()
+                .body()
+                .statusCode(204);
+    }
+
+    @Test
+    public void ct134_naoDeveExcluirContatoInexistente() {
+        given()
+                .log()
+                .all()
+                .when()
+                .delete("https://api-de-tarefas.herokuapp.com/contacts/9999")
+                .then()
+                .log()
+                .body()
+                .statusCode(404);
+    }
 
 }
